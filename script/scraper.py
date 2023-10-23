eb01cd09563ec11dba96f40491de70b0d99bc603
Yuri Machado <ymachado1995@gmail.com>
2023-10-23 15:15:48 -0300

    feat: :zap: extracting movie details
    created the function to extract the movie details

--- script/scraper.py
+++ script/scraper.py
@@ -11,6 +11,42 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 
 MAX_THREADS = 10
 
+def extract_movie_details(movie_link):
+    global processed_count
+    processed_count += 1
+    time.sleep(random.uniform(0, 0.2))
+    try:
+        response = BeautifulSoup(requests.get(movie_link, headers=headers).content, 'html.parser')
+        movie_soup = response
+    except requests.exceptions.RequestException as e:
+        print(f'Erro na solicitação HTTP: {e}')
+    except Exception as e:
+        print(f'Erro na análise do HTML: {e}')
+        movie_soup = None
+
+
+    if movie_soup is not None:
+
+        title = movie_soup.find('h1', attrs={'class': 'sc-afe43def-0 hnYaOZ'}).get_text()
+        popularity = movie_soup.find('div', attrs={'data-testid': 'hero-rating-bar__popularity__score'}).get_text()
+        rating = movie_soup.find('span', attrs={'class': 'sc-bde20123-1 iZlgcd'}).get_text()  if movie_soup.find('span', attrs={'class': 'sc-bde20123-1 iZlgcd'}) else None
+        plot_text = movie_soup.find('span', attrs={'class': 'sc-466bb6c-0 kJJttH'}).get_text().strip() if movie_soup.find('span', attrs={'class': 'sc-466bb6c-0 kJJttH'}) else None
+
+
+        # print(["Titulo: ", title,"Data: ", popularity, "Rating: ", rating,"Descrição: ", plot_text])
+
+        with open('movies.csv', mode='a') as file:
+            movie_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            if all([title, popularity, rating, plot_text]):
+                # print('Gravando dados no CSV', title, popularity, rating, plot_text)
+                movie_writer.writerow([processed_count, title, popularity, rating, plot_text])
+                print(f'O total de filmes processados:{processed_count}')  
+            else:
+                print("Dados ausentes, Não gravados no CSV", ["Titulo: ", title,"Data: ", popularity, "Rating: ", rating,"Descrição: ", plot_text])
+    else: 
+        print('problema ao ler HTML')
+
+
 def extract_movies(soup):
 
     movies_table = soup.find('div', attrs={'data-testid': 'chart-layout-main-column'}).find('ul')
